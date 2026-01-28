9352f5d04d622f375eaa72dd648466b3f4bcf876
Dylan-Ay <dylan.ayache.68@gmail.com>
2026-01-28 17:40:51 +0100

    Création de CategorySection, CategoryButton, categoryColors pour la section categories de la HomeScreen + utils.js pour les fonctions utilitaires

---
+++ frontend/src/components/ui/inputs/CategoryButton.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, TouchableOpacity} from "react-native"
+import { CATEGORY_COLORS } from "../../../constants/categoryColors"
+
+export const CategoryChip = ({ id, name, icon }) => {
+   const backgroundColor = CATEGORY_COLORS[id] ?? '#D0EBFF';
+   
+   return(
+      <TouchableOpacity style={[styles.chip, { backgroundColor }]}>
+         <Text style={styles.name}>{icon} {name}</Text>
+      </TouchableOpacity>
+   )
+}
+
+const styles = StyleSheet.create({
+   chip: {
+      alignItems: 'center',
+      padding: 20,
+      margin: 5,
+      borderRadius: 30
+   },
+
+   name: {
+      fontSize: 16,
+      fontWeight: '500'
+   }
+})

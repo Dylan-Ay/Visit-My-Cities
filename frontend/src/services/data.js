493ea6aa3b9ef2e3261b6ef739e68640b01666f4
Dylan-Ay <dylan.ayache.68@gmail.com>
2026-01-28 17:45:44 +0100

    Séparation des données temporaires dans un fichier à part

---
+++ frontend/src/services/data.js
@@ -0,0 +1,27 @@
+// Exemple de données
+export const popularCities = [
+  { id: '1', name: 'Paris', image: 'https://www.secretdeparis.com/blog/wp-content/uploads/2025/04/eiffel-tower-975004_1280.jpg' },
+  { id: '2', name: 'Tokyo', image: 'https://cdn-blog.superprof.com/blog_fr/wp-content/uploads/2018/03/palais-imperial-tokyo.jpg' },
+  { id: '3', name: 'New York', image: 'https://cdn-imgix.headout.com/tour/30357/TOUR-IMAGE/6cdcf542-452d-4897-beed-76cf68f154e4-1act-de005e04-05d9-4715-96b0-6a089d5c3460.jpg?auto=format&w=1222.3999999999999&h=687.6&q=90&ar=16%3A9&crop=faces&fit=crop' },
+  { id: '4', name: 'Londres', image: 'https://media.clondres.com/resize/uploads/sites/5/2018/03/big-ben-londres-696x478.jpg?format=auto' },
+  { id: '5', name: 'Rome', image: 'https://www.webcity.fr/wp-content/uploads/2017/11/monument-rome-1.jpg' },
+  { id: '6', name: 'Barcelone', image: 'https://jacheteenespagne.com/wp-content/uploads/2024/06/0bda496746574de3ac1ec5c68616272b-1.jpg' },
+];
+
+export const popularBuildings = [
+  { id: '1', cityId: '1', name: 'Musée du Louvre', image: 'https://i.notretemps.com/2000x1125/smart/2023/10/19/musee-du-louvre.jpg' },
+  { id: '2', cityId: '2', name: 'Sanctuaire Asakusa', image: 'https://media.istockphoto.com/id/1329537420/fr/photo/tokyo-japon-27-octobre-2017-tokyo-japon-horizon-de-la-ville-au-temple-asakusa.jpg?s=612x612&w=0&k=20&c=0mRZPy-eg603K8FPB7k_O73e7tqHLv4PIDjWIKcXXH4=' },
+  { id: '3', cityId: '3', name: 'Empire State Building', image: 'https://cdn.calendarz.com/uploads/events/may/1/41113/empire-state-building_compressed.jpg' },
+  { id: '4', cityId: '4', name: 'Buckingham Palace', image: 'https://cdn-imgix.headout.com/media/images/31a4bf553f447246bf5bd92b00cb1707-12286-london-changing-of-the-guard-tour-with-optional-buckingham-palace-entry-11.jpg?auto=compress%2Cformat&w=1216.3200000000002&h=760.2&q=210&ar=16%3A10&crop=faces&fit=crop' },
+  { id: '5', cityId: '5', name: 'Fontaine de Trevi', image: 'https://as2.ftcdn.net/v2/jpg/01/21/87/47/1000_F_121874733_ObMVN0dprpejLPmvkQumpLRvxOWOI7sW.jpg' },
+  { id: '6', cityId: '6', name: 'Sagrada Família', image: 'https://lelephant-larevue.fr/wp-content/uploads/2018/03/%CE%A3%CE%B1%CE%B3%CF%81%CE%B1%CC%81%CE%B4%CE%B1_%CE%A6%CE%B1%CE%BC%CE%B9%CC%81%CE%BB%CE%B9%CE%B1_2941.jpg' },
+];
+
+export const categories = [
+   { id: '1', name: 'Musées', icon: '🏛' },
+   { id: '2', name: 'Ponts', icon: '🌉' },
+   { id: '3', name: 'Religieux', icon: '⛪' },
+   { id: '4', name: 'Gratte-ciels', icon: '🏙' },
+   { id: '5', name: 'Châteaux & palais', icon: '🏰' },
+   { id: '6', name: 'Bâtiments civils', icon: '🏢' },
+];

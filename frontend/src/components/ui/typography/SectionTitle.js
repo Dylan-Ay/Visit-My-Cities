ac6ebd3cb1c29806d3b9db89c4aa368a0575ec80
Dylan-Ay <dylan.ayache.68@gmail.com>
2026-02-08 19:09:22 +0100

    Update de la fontSize de SectionTitle + ajout d'un prop style pour définir la fontSize

--- frontend/src/components/ui/typography/SectionTitle.js
+++ frontend/src/components/ui/typography/SectionTitle.js
@@ -1,16 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 
-export const SectionTitle = ({ children }) => {
+export const SectionTitle = ({ children, style }) => {
    return (
       <View>
-         <Text style={styles.sectionTitle}>{children}</Text>
+         <Text style={[styles.sectionTitle, style]}>{children}</Text>
       </View>
    )
 }
 
 const styles = StyleSheet.create({
    sectionTitle: {
-      fontSize: 26,
+      fontSize: 24,
       fontWeight: '600',
       color: '#222222',
       paddingTop: 20,

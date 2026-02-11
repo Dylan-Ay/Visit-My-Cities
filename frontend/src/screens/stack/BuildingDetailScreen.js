5c7a77afbd2ecdd3ba437106045d0c655f41fb30
Dylan-Ay <dylan.ayache.68@gmail.com>
2026-02-11 15:07:53 +0100

    Création des composants VisitInfoSection + VisitInfosGrid + VisitInfoCard + intégration de VisitInfoSection à BuildingDetail

--- frontend/src/screens/stack/BuildingDetailScreen.js
+++ frontend/src/screens/stack/BuildingDetailScreen.js
@@ -1,13 +1,12 @@
 // prettier-ignore
-import { ScrollView, StyleSheet, Text, View, } from 'react-native'
-// prettier-ignore
-import { ContentContainer, HeroBanner, ScreenWrapper, SectionTitle, SectionDivider } from '../../components/ui'
+import { ContentContainer, HeroBanner, ScreenWrapper, SectionDivider } from '../../components/ui'
+import { ScrollView } from 'react-native'
 import { buildings } from '../../services/data'
-import { Ionicons } from '@expo/vector-icons'
 import KeyInfosSection from '../../components/sections/KeyInfosSection/KeyInfosSection'
 import DescriptionSection from '../../components/sections/DescriptionSection'
+import VisitInfoSection from '../../components/sections/VisitInfoSection/VisitInfoSection'
 
-export const BuildingDetailScreen = ({ navigation, route }) => {
+export const BuildingDetailScreen = ({ route }) => {
    const { buildingId } = route.params
    const building = buildings.find((element) => element.id == buildingId)
 
@@ -28,175 +27,11 @@ export const BuildingDetailScreen = ({ navigation, route }) => {
 
                <SectionDivider />
 
-               <SectionTitle style={{ fontSize: 22 }}>
-                  Informations de Visite
-               </SectionTitle>
-               <View
-                  style={{
-                     gap: 12,
-                     flexDirection: 'row',
-                     flexWrap: 'wrap',
-                  }}
-               >
-                  <View
-                     style={{
-                        flexDirection: 'row',
-                        gap: 6,
-                        backgroundColor: 'white',
-                        alignItems: 'center',
-                        borderRadius: 16,
-                        padding: 8,
-                        shadowColor: '#000',
-                        shadowOffset: {
-                           width: 0,
-                           height: 1,
-                        },
-                        shadowOpacity: 0.2,
-                        shadowRadius: 1.41,
-                        elevation: 2,
-                        width: '48%',
-                     }}
-                  >
-                     <Ionicons style={styles.icon} name="time-outline" />
-                     <View style={{ gap: 4 }}>
-                        <Text style={{ fontWeight: 600 }}>Horaires</Text>
-                        <Text>9:30 - 22:45</Text>
-                     </View>
-                  </View>
-
-                  <View
-                     style={{
-                        flexDirection: 'row',
-                        gap: 6,
-                        backgroundColor: 'white',
-                        alignItems: 'center',
-                        borderRadius: 16,
-                        padding: 8,
-                        shadowColor: '#000',
-                        shadowOffset: {
-                           width: 0,
-                           height: 1,
-                        },
-                        shadowOpacity: 0.2,
-                        shadowRadius: 1.41,
-                        elevation: 2,
-                        width: '48%',
-                     }}
-                  >
-                     <Ionicons style={styles.icon} name="pricetag-outline" />
-                     <View style={{ gap: 4 }}>
-                        <Text style={{ fontWeight: 600 }}>Tarif</Text>
-                        <Text>
-                           {building.ticketPrice === 0
-                              ? 'Gratuit'
-                              : building.ticketPrice + '€'}
-                           {}
-                        </Text>
-                     </View>
-                  </View>
-
-                  <View
-                     style={{
-                        flexDirection: 'row',
-                        gap: 6,
-                        backgroundColor: 'white',
-                        alignItems: 'center',
-                        borderRadius: 16,
-                        padding: 8,
-                        shadowColor: '#000',
-                        shadowOffset: {
-                           width: 0,
-                           height: 1,
-                        },
-                        shadowOpacity: 0.2,
-                        shadowRadius: 1.41,
-                        elevation: 2,
-                        width: '48%',
-                     }}
-                  >
-                     <Ionicons style={styles.icon} name="hourglass-outline" />
-                     <View style={{ gap: 4 }}>
-                        <Text style={{ fontWeight: 600 }}>Temps de visite</Text>
-                        <Text>{building.visitDuration}</Text>
-                     </View>
-                  </View>
-
-                  <View
-                     style={{
-                        flexDirection: 'row',
-                        gap: 6,
-                        backgroundColor: 'white',
-                        alignItems: 'center',
-                        borderRadius: 16,
-                        padding: 8,
-                        shadowColor: '#000',
-                        shadowOffset: {
-                           width: 0,
-                           height: 1,
-                        },
-                        shadowOpacity: 0.2,
-                        shadowRadius: 1.41,
-                        elevation: 2,
-                        width: '48%',
-                     }}
-                  >
-                     <Ionicons
-                        style={styles.icon}
-                        name="accessibility-outline"
-                     />
-                     <View style={{ gap: 4 }}>
-                        <Text style={{ fontWeight: 600 }}>Accessibilité</Text>
-                        <Text>
-                           {building.accessiblePRM
-                              ? 'Accessible'
-                              : 'Non accessible'}
-                        </Text>
-                     </View>
-                  </View>
-               </View>
+               <VisitInfoSection building={building} />
             </ContentContainer>
+            {/* Horaires */}
             {/* Map localisation */}
          </ScrollView>
       </ScreenWrapper>
    )
 }
-
-const styles = StyleSheet.create({
-   container: {
-      borderRadius: 16,
-      backgroundColor: 'white',
-      flexDirection: 'row',
-      shadowColor: '#000',
-      shadowOffset: {
-         width: 0,
-         height: 1,
-      },
-      shadowOpacity: 0.2,
-      shadowRadius: 1.41,
-      elevation: 2,
-      marginBottom: 28,
-   },
-   container2: {
-      borderRadius: 16,
-      backgroundColor: 'white',
-      flexDirection: 'row',
-      shadowColor: '#000',
-      shadowOffset: {
-         width: 0,
-         height: 1,
-      },
-      shadowOpacity: 0.2,
-      shadowRadius: 1.41,
-      elevation: 2,
-      marginBottom: 28,
-   },
-   keysInfosContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      padding: 8,
-      gap: 6,
-   },
-   icon: {
-      fontSize: 22,
-   },
-})

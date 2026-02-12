e5b7bccc90f0b76d01df11fabf148f8377ecea19
Dylan-Ay <dylan.ayache.68@gmail.com>
2026-02-12 20:23:49 +0100

    Vérification avec un ternaire si un horaire est disponible par jour sinon afficher "Fermé" + utilisation de firstChartToUpperCase pour un jour

--- frontend/src/components/sections/SchedulesSection.js
+++ frontend/src/components/sections/SchedulesSection.js
@@ -1,25 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native'
 import { Accordion } from '../ui'
+import { firstChartToUpperCase } from '../../utils/utils'
 
 export default function SchedulesSection({ buildingSchedules }) {
    const schedulesDays = buildingSchedules.days
    const scheduleType = buildingSchedules.type
    const scheduleNote = buildingSchedules.note
 
-   console.log(
-      Object.entries(schedulesDays).map(([day, slots]) =>
-         console.log(day + ' ' + slots)
-      )
-   )
    // Vérifier le type pour afficher ou non les horaires
    return (
       <View>
          <Accordion title={'Horaires'}>
             {Object.entries(schedulesDays).map(([day, slots]) => (
                <View style={styles.schedulesRow} key={day}>
-                  <Text>{day}</Text>
+                  <Text style={styles.day}>{firstChartToUpperCase(day)}</Text>
                   <Text>
-                     {slots[0].start} - {slots[0].end}
+                     {slots.length > 0
+                        ? `${slots[0].start} - ${slots[0].end}`
+                        : 'Fermé'}
                   </Text>
                </View>
             ))}
@@ -32,6 +30,9 @@ const styles = StyleSheet.create({
    schedulesRow: {
       flexDirection: 'row',
       justifyContent: 'space-between',
-      paddingVertical: 12,
+      paddingVertical: 10,
+   },
+   day: {
+      fontWeight: 600,
    },
 })

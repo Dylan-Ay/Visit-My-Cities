b301c09b5d10a7fdce2e9283c16c8f17684c8484
Dylan-Ay <dylan.ayache.68@gmail.com>
2026-02-12 16:28:23 +0100

    Ajout à SectionTitle des props "right" et "containerStyle" pour l'utilisation dans un Accordion

--- frontend/src/components/ui/typography/SectionTitle.js
+++ frontend/src/components/ui/typography/SectionTitle.js
@@ -1,19 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native'
 
-export const SectionTitle = ({ children, style }) => {
+export const SectionTitle = ({ children, containerStyle, style, right }) => {
    return (
-      <View>
+      <View style={[styles.container, containerStyle]}>
          <Text style={[styles.sectionTitle, style]}>{children}</Text>
+         {right}
       </View>
    )
 }
 
 const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      paddingTop: 20,
+      paddingBottom: 12,
+   },
    sectionTitle: {
       fontSize: 24,
       fontWeight: '600',
       color: '#222222',
-      paddingTop: 20,
-      paddingBottom: 12,
    },
 })

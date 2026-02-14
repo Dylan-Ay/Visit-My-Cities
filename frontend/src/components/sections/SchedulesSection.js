6318e926f4fe82a64c8df6343de6fc0e96781a04
Dylan-Ay <dylan.ayache.68@gmail.com>
2026-02-14 15:44:48 +0100

    Fin de la section SchedulesSection : Gestion de l'affichage des éléments selon le schedulesType + Ajout de la schedulesNote et de l'url des horaires du bâtiment

--- frontend/src/components/sections/SchedulesSection.js
+++ frontend/src/components/sections/SchedulesSection.js
@@ -1,26 +1,48 @@
 import { StyleSheet, Text, View } from 'react-native'
 import { Accordion } from '../ui'
 import { firstChartToUpperCase } from '../../utils/utils'
+import { Linking } from 'react-native'
 
 export default function SchedulesSection({ buildingSchedules }) {
    const schedulesDays = buildingSchedules.days
-   const scheduleType = buildingSchedules.type
-   const scheduleNote = buildingSchedules.note
+   const schedulesType = buildingSchedules.type
+   const schedulesNote = buildingSchedules.note
+   const schedulesUrl = buildingSchedules.officialHoursUrl
 
-   // Vérifier le type pour afficher ou non les horaires
    return (
       <View>
          <Accordion title={'Horaires'}>
-            {Object.entries(schedulesDays).map(([day, slots]) => (
-               <View style={styles.schedulesRow} key={day}>
-                  <Text style={styles.day}>{firstChartToUpperCase(day)}</Text>
-                  <Text>
-                     {slots.length > 0
-                        ? `${slots[0].start} - ${slots[0].end}`
-                        : 'Fermé'}
+            {schedulesType != 'Variable' &&
+               Object.entries(schedulesDays).map(([day, slots]) => (
+                  <View style={styles.schedulesRow} key={day}>
+                     <Text style={styles.day}>
+                        {firstChartToUpperCase(day)}
+                     </Text>
+                     <Text>
+                        {slots.length > 0
+                           ? `${slots[0].start} - ${slots[0].end}`
+                           : 'Fermé'}
+                     </Text>
+                  </View>
+               ))}
+
+            <View style={styles.noteContainer}>
+               {schedulesType == 'Variable' && (
+                  <Text style={styles.variableSchedules}>
+                     Horaires Variables
                   </Text>
-               </View>
-            ))}
+               )}
+               <Text style={styles.note}>{schedulesNote}</Text>
+               {schedulesUrl && (
+                  <Text
+                     style={styles.link}
+                     onPress={() => Linking.openURL(schedulesUrl)}
+                  >
+                     Consultez le site pour vérifier les horaires avant votre
+                     visite.
+                  </Text>
+               )}
+            </View>
          </Accordion>
       </View>
    )
@@ -34,5 +56,24 @@ const styles = StyleSheet.create({
    },
    day: {
       fontWeight: 600,
+   },
+   noteContainer: {
+      padding: 16,
+      marginTop: 10,
+      backgroundColor: 'rgb(230, 230, 230)',
+      borderRadius: 16,
+      gap: 10,
+   },
+   note: {
+      color: '#393939',
+   },
+   link: {
+      fontWeight: 500,
+      color: '#393939',
+      textDecorationLine: 'underline',
+   },
+   variableSchedules: {
+      fontWeight: 600,
+      fontSize: 16,
    },
 })

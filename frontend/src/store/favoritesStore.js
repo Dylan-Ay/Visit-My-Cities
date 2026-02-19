6b5091fc9dfb6c0478a3e5a1cafc3ea249fb646f
Dylan-Ay <dylan.ayache.68@gmail.com>
2026-02-19 16:13:18 +0100

    Correction import AsyncStorage dans favoritesStore

--- frontend/src/store/favoritesStore.js
+++ frontend/src/store/favoritesStore.js
@@ -1,6 +1,6 @@
 import { create } from 'zustand'
 import { createJSONStorage, persist } from 'zustand/middleware'
-import { AsyncStorage } from '@react-native-async-storage/async-storage'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export const useFavorite = create(
    persist(

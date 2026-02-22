a805b3ca459ce2c21a5e07b513c35bd8219ea56f
Dylan-Ay <dylan.ayache.68@gmail.com>
2026-02-22 18:21:53 +0100

    Fin de LoginScreen, avec intégration de HeaderSection, des FormInput et de AuthActionsSection

--- frontend/src/screens/stack/LoginScreen.js
+++ frontend/src/screens/stack/LoginScreen.js
@@ -1,15 +1,57 @@
-import {
-   ContentContainer,
-   ScreenWrapper,
-   SectionTitle,
-} from '../../components/ui'
+import { ScrollView, StyleSheet } from 'react-native'
+import { ContentContainer, FormInput, ScreenWrapper } from '../../components/ui'
+import { View } from 'react-native'
+import AuthActionsSection from '../../components/sections/AuthActionsSection'
+import { HeaderSection } from '../../components/sections/HeaderSection'
 
-export const LoginScreen = ({}) => {
+export const LoginScreen = ({ navigation }) => {
    return (
       <ScreenWrapper>
-         <ContentContainer style={{ flex: 1 }}>
-            <SectionTitle>Connexion</SectionTitle>
-         </ContentContainer>
+         <ScrollView
+            showsVerticalScrollIndicator={false}
+            keyboardShouldPersistTaps="handled"
+         >
+            <ContentContainer>
+               <HeaderSection
+                  title={'Connexion'}
+                  subTitle={'Connectez-vous à Visit My Cities'}
+               />
+
+               <View style={styles.inputsContainer}>
+                  <FormInput
+                     label={'Adresse email'}
+                     placeholder={'Insérez votre adresse email'}
+                     keyboardType={'email-address'}
+                     icon={'close-outline'}
+                     returnKeyType={'next'}
+                  />
+                  <FormInput
+                     label={'Mot de passe'}
+                     placeholder={'Insérez votre mot de passe'}
+                     keyboardType={'default'}
+                     secureTextEntry={true}
+                     icon={'close-outline'}
+                     returnKeyType={'send'}
+                  />
+               </View>
+
+               <AuthActionsSection
+                  primaryTitle={'Se Connecter'}
+                  primaryOnPress={() =>
+                     navigation.navigate('Tabs', { screen: 'Profil' })
+                  }
+                  secondaryTitle={'Créer un compte'}
+                  secondaryOnPress={() => navigation.navigate('RegisterScreen')}
+               />
+            </ContentContainer>
+         </ScrollView>
       </ScreenWrapper>
    )
 }
+
+const styles = StyleSheet.create({
+   inputsContainer: {
+      gap: 24,
+      paddingBottom: 20,
+   },
+})

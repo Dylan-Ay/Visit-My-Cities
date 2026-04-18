9bf0f451f75e8d73a6cf5c17391ea3223bbaec85
Dylan-Ay <dylan.ayache.68@gmail.com>
2026-04-18 22:28:42 +0200

    Passage du store useUserStore de JS à TS + initialisation de user à "null"

--- frontend/src/store/useUserStore.ts
+++ frontend/src/store/useUserStore.ts
@@ -2,14 +2,24 @@ import { create } from 'zustand'
 import { createJSONStorage, persist } from 'zustand/middleware'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
-export const useUserStore = create(
+type User = {
+   username: string | null
+   email: string | null
+   role: 'ROLE_VISITEUR' | 'ROLE_EXPERT'
+}
+
+interface UserState {
+   user: User | null
+   token: string | null
+   setUser: (payload: { user: User; token: string }) => void
+   logout: () => void
+   isLoggedIn: () => boolean
+}
+
+export const useUserStore = create<UserState>()(
    persist(
       (set, get) => ({
-         user: {
-            username: null,
-            email: null,
-            role: 'ROLE_VISITEUR',
-         },
+         user: null,
          token: null,
 
          setUser: ({ user, token }) => set({ user, token }),
